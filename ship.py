78426ca856081d795906bf420b144f8d2f66915b
Tiger Zhu <143968664+TigerZhu10@users.noreply.github.com>
2024-09-15 11:39:26 -0700

    Test

--- ship.py
+++ ship.py
@@ -6,19 +6,20 @@ import pygame
 '''
 class Ship:
     def __init__(self, display_screen, game_settings):
+        self.game_settings = game_settings
         self.screen = display_screen
         self.ship_image = pygame.image.load("./assets/images/player_ship.png")
         self.ship_image_rect = self.ship_image.get_rect()
-        self.ship_image_rect.centerx = game_settings.WINDOW_WIDTH//2
-        self.ship_image_rect.bottom = game_settings.WINDOW_HEIGHT
+        self.ship_image_rect.centerx = self.game_settings.WINDOW_WIDTH//2
+        self.ship_image_rect.bottom = self.game_settings.WINDOW_HEIGHT
 
         self.moving_right = False
         self.moving_left = False
 
     def moving_ship(self):
-        if self.moving_right:
+        if self.moving_right and self.ship_image_rect.right <= self.game_settings.WINDOW_WIDTH:
             self.ship_image_rect.centerx += 1
-        if self.moving_left:
+        if self.moving_left and self.ship_image_rect.left >= 0:
             self.ship_image_rect.centerx -= 1
 
     def display_ship(self):
