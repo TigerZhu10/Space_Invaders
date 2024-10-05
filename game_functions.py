5a8e57e67e7d3d4b7adf88bfef92239276cc5ff5
Tiger Zhu <143968664+TigerZhu10@users.noreply.github.com>
2024-10-05 10:51:21 -0700

    packed functions
    lunch_bullet
    bullet_group_display

--- game_functions.py
+++ game_functions.py
@@ -9,9 +9,7 @@ def Key_up(ship, event):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
-
-
-
+    
 def Key_down(ship, event, bullet_group, screen, game_settings):
 
     #当按键被按下的时候把flag变成True来连续移动飞船
@@ -20,9 +18,8 @@ def Key_down(ship, event, bullet_group, screen, game_settings):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True 
 
-    elif event.key == pygame.K_SPACE:
-        if len(bullet_group) < game_settings.bullet_num_allowed:
-            bullet_group.add(Bullet(screen, ship, game_settings))
+    if event.key == pygame.K_SPACE:
+        lunch_bullet(bullet_group, game_settings, screen, ship)
 
 
 def check_mouse_key_events(ship, screen, bullet_group, game_settings):
@@ -30,9 +27,9 @@ def check_mouse_key_events(ship, screen, bullet_group, game_settings):
         if ev.type == pygame.QUIT:
             sys.exit()
 
-        if ev.type == pygame.KEYDOWN:
+        elif ev.type == pygame.KEYDOWN:
             Key_down(ship, ev, bullet_group, screen, game_settings)
-        if ev.type == pygame.KEYUP:
+        elif ev.type == pygame.KEYUP:
             Key_up(ship, ev)  
               
 
@@ -46,7 +43,7 @@ def update_screen(display_screen, ship, game_settings, bullet_group):
         
         # 更新所有活动
         pygame.display.flip()
-        
+
 
 def bullet_group_display(bullet_group):
     for bullet in bullet_group.sprites():
@@ -56,3 +53,7 @@ def bullet_group_display(bullet_group):
     for bullet in bullet_group.copy():
         if bullet.rect.bottom <= 0:
             bullet_group.remove(bullet)
+
+def lunch_bullet(bullet_group, game_settings, screen, ship):
+        if len(bullet_group) < game_settings.bullet_num_allowed:
+            bullet_group.add(Bullet(screen, ship, game_settings))
